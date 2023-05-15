321a797bc33178bb8cf240f334407bcc043d3954
whaleshade <whaleshade@gmail.com>
2023-05-15 22:28:17 +0900

    스프링빈 방식으로 변경

--- spring-1/src/main/java/hello/hellospring/service/MemberService.java
+++ spring-1/src/main/java/hello/hellospring/service/MemberService.java
@@ -9,11 +9,12 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
+//@Service
 public class MemberService {
     private final MemberRepository memberRepository;
-    @Autowired
+//    @Autowired
     public MemberService(MemberRepository memberRepository) {
+
         this.memberRepository = memberRepository; // 의존성 주입
     }
 
