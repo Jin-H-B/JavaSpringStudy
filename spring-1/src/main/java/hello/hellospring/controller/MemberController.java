57e94a11c98027578348843198e2178da5f356ac
whaleshade <whaleshade@gmail.com>
2023-05-16 17:28:12 +0900

    회원목록 조회

--- spring-1/src/main/java/hello/hellospring/controller/MemberController.java
+++ spring-1/src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -32,5 +35,12 @@ public class MemberController {
         System.out.println("member = " + member.getName());
 
         return "redirect:/"; //redirect 시킴
+    }
+
+    @GetMapping("/members") //Model model을 통해 db에 저장된 데이터를 controller를 통해 view로 넘기게 됨
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members); // "members"라는 이름으로 멤버 목록을 모델에 추가. 뷰에서 해당 데이터를 참조할 수 있게 됨.
+        return "members/memberList";
     }
 }
